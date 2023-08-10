c48f0d13245d9c0610708ed0fc551e39f50cb07a
Sathyaraj06 <46516308+Sathyaraj06@users.noreply.github.com>
2023-08-10 06:37:11 +0000

    Update mysql-cron.sh

--- mysql/mysql-cron.sh
+++ mysql/mysql-cron.sh
@@ -2,7 +2,9 @@ chmod +x /mysql-backup.sh
 
 crontab -e
 
-# 0 2 * * * root /mysql-backup.sh
+ 0 0 * * * /home/ubuntu/rasa-evis/support/mysql_backup/mysql-backup.sh
+ 0 1 * * * /home/ubuntu/EvisMonitor/LinuxExe/EvisMonitor
+
 
 SET SQL_SAFE_UPDATES = 0;
 delete FROM evis.exceptionlog;
