5eb5e17a129b4c7ec913597a4b476ff118abf154
Sathyaraj06 <46516308+Sathyaraj06@users.noreply.github.com>
2023-08-11 07:23:19 +0000

    Update mysql-cron.sh

--- mysql/mysql-cron.sh
+++ mysql/mysql-cron.sh
@@ -2,8 +2,8 @@ chmod +x /mysql-backup.sh
 
 crontab -e
 
- 0 0 * * * /home/ubuntu/rasa-evis/support/mysql_backup/mysql-backup.sh
- 0 1 * * * /home/ubuntu/EvisMonitor/LinuxExe/EvisMonitor
+ 0 0 * * * /home/ubuntu/LinuxSetup/mysql/mysql-backup.sh
+ 0 1 * * * . $HOME/.profile; /home/ubuntu/EvisMonitor/LinuxExe/EvisMonitor 2>&1 | /usr/bin/logger -t tag1
 
  */5 * * * * /home/ubuntu/LinuxSetup/mysql/mysql-backup.sh
 
@@ -12,6 +12,8 @@ sudo service cron reload
 # checking other users cron jobs
 crontab -u ubuntu -l
 crontab -u ubuntu -e
+
+. $HOME/.bash_profile
 
 # to see history
 cat /var/log/syslog
